88eb22bf1df0fb9358f258c2333f82c26676954e
Rosario <rosariogomez639@gmail.com>
2022-09-27 17:52:16 -0300

    refactor code

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const list = document.querySelector('#list');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    const product = this.elements.product.value;
-    const qty = this.elements.qty.value;
-    console.log(product, qty);
+    const product = this.elements.product;
+    const qty = this.elements.qty;
+    addProduct(product.value, qty.value); 
+    product.value = ""; 
+    qty.value = ""; 
+}); 
 
+const addProduct = (product, qty) => {
     const newGroceryItem = document.createElement('li');
     newGroceryItem.append(qty);  
     newGroceryItem.append(` ${product}`); 
     list.append(newGroceryItem); 
-}); 
+}
